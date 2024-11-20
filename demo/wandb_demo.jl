8129353f5012dfde45b39bb91600906805836bcc
Zhehao Ma <zhehaoma@mrt.kit.edu>
2024-11-20 17:25:55 +0100

    feat: use wandb to track training process

---
+++ demo/wandb_demo.jl
@@ -0,0 +1,19 @@
+using Wandb, Logging, Dates
+
+lg = WandbLogger(
+    project = "VectorLanelet",
+    name = "demo-$(now())",
+    config = Dict("learning_rate" => 0.01, "dropout" => 0.5)
+)
+
+global_logger(lg)
+
+with_logger(lg) do
+    for x in 1:50
+        acc = log(1 + x + rand() * get_config(lg, "learning_rate") + rand())
+        loss = 10 - log(1 + x + rand() + x * get_config(lg, "learning_rate") + rand())
+        # Log metrics from your script to W&B
+        @info "metrics" accuracy=acc loss=loss
+        @info "foo" x=x
+    end
+end
