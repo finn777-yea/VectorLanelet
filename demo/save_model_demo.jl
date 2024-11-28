71d5e7860b8feb8b018fbd2efc361a20155d5830
Zhehao Ma <zhehaoma@mrt.kit.edu>
2024-11-28 21:12:19 +0100

    feat: add demo save and load model state

---
+++ demo/save_model_demo.jl
@@ -0,0 +1,24 @@
+using VectorLanelet
+using Flux
+using BSON
+
+include("../src/config.jl")
+model = LaneletPredictor(config)
+
+# ------ BSON -------
+# Save the model
+model_path = joinpath(@__DIR__, "../res/model.bson")
+BSON.@save model_path model
+
+# Load the model
+model = BSON.@load model_path
+
+# ------ JLD2 -------
+using JLD2
+model_state = Flux.state(model)
+model_state_path = joinpath(@__DIR__, "../res/model_state.jld2")
+jldsave(model_state_path; model_state)
+
+loaded_model_state = JLD2.load(model_state_path, "model_state")
+model = LaneletPredictor(config)
+Flux.loadmodel!(model, loaded_model_state)
