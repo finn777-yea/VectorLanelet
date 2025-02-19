4bd828fda10abfeceeb9abc7564e2d578df8cf79
Zhehao Ma <zhehaoma@mrt.kit.edu>
2025-02-19 14:54:18 +0100

    Test: test interaction graph construction

--- test/test_interaction_graph.jl
+++ test/test_interaction_graph.jl
@@ -3,54 +3,43 @@ using VectorLanelet: load_map_data, prepare_agent_features, create_filtered_inte
 using GraphNeuralNetworks
 using Random
 
+"""
+    Position of agents and lanelets:
+                         y
+                         ^
+                         |
+                         |                   
+                         |
+---l5---l4---l3---l2---l1/a1---a2---a3---a4--->  x
+                         |
+"""
 
 @testset "Interaction Graph" begin
-    lanelet_roadway, g_meta = load_map_data()
-    agt_features, agt_pos, labels = prepare_agent_features(lanelet_roadway)
+    agt_pos = [Float32[i, 0] for i in 0:3]
+    llt_pos = [Float32[-i, 0] for i in 0:4]
 
-    llt_pos = rand(Float32, (2, 128))
+    agt_pos = hcat(agt_pos...)
+    llt_pos = hcat(llt_pos...)
     n_in = 64        # Number of features for each node
     e_in = 1
     out_dim = 64
     num_heads = 4
     num_layers = 2
     num_agents = size(agt_pos, 2)
+    num_llts = size(llt_pos, 2)
     agent_emb = rand(Float32, n_in, num_agents)
-    map_emb = rand(Float32, n_in, size(llt_pos, 2))
+    map_emb = rand(Float32, n_in, num_llts)
+    dist_thrd = 2.0
 
     @testset "create_filtered_interaction_graphs with features" begin
-        g_agent = create_filtered_interaction_graphs(agt_pos, llt_pos, 100.0)
-        @test all(g_agent.edata.d .< 100.0)
-        @test typeof(g_agent.edata.d) == Matrix{Float32}
+        g_agent = create_filtered_interaction_graphs(agt_pos, llt_pos, dist_thrd)
+        @test all(g_agent.edata.d .< dist_thrd)
+        @assert typeof(g_agent.edata.d) == Matrix{Float32}
+        @show edge_index(g_agent)
     end
-    
-    # @testset "create_filtered_interaction_graphs without features" begin
-        #     g_agent = create_filtered_interaction_graphs(agt_pos, llt_pos, 100.0)
-        #     @test g_agent.num_graphs == num_agents
-        #     @test all(g_agent.edata.d .< 100.0)
-        # end
-        
-    # DEPRECATED!!!
-    # @testset "prepare_interaction_feautures" begin
-    #     using VectorLanelet: prepare_interaction_feautures
-    #     emb_agt = [0.1 0.2; 0.1 0.2]
-    #     emb_ctx = [1.0 2.0 3.0; 1.0 2.0 3.0]
 
-    #     agt_g1 = star_digraph(3) |> GNNGraph
-    #     agt_g1.ndata.ind = [1, 1, 2]    # 1 is agt, 2, 3 are ctx nodes
-    #     agt_g2 = star_digraph(2) |> GNNGraph
-    #     agt_g2.ndata.ind = [2, 3]    # 2 is agt, 3 is ctx node
-    #     g_all = batch([agt_g1, agt_g2])
-
-        
-    #     expected_ndata = Float32[0.1 1.0 2.0 0.2 3.0; 0.1 1.0 2.0 0.2 3.0]
-    #     res = prepare_interaction_feautures(emb_agt, emb_ctx, g_all)
-    #     @show eltype(res)
-    #     @test res == expected_ndata
+    # @testset "InteractionGraphModel" begin
+    #     interaction = InteractionGraphModel(n_in, e_in, out_dim, num_heads=num_heads)
+    #     res = interaction(agent_emb, agt_pos, map_emb, llt_pos, 100.0)
     # end
-
-    @testset "InteractionGraphModel" begin
-        interaction = InteractionGraphModel(n_in, e_in, out_dim, num_heads=num_heads)
-        res = interaction(agent_emb, agt_pos, map_emb, llt_pos, 100.0)
-    end
 end
