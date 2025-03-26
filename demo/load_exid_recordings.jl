1b9a35b1f1aa99d54aa80b3e0eb364aa2c601d4d
Zhehao Ma <zhehaoma@mrt.kit.edu>
2025-03-26 12:47:05 +0100

    Chore: rename load_recordings.jl

--- demo/load_exid_recordings.jl
+++ demo/load_exid_recordings.jl
@@ -8,6 +8,14 @@ recording_ids = [0,1,2,3,4,5]
 data_infos = ExiDRecordings(recording_ids, dataset_path)
 dataset = read(data_infos)
 
+### Included vehicle features
+# vehicle.xCenter,        # x position
+# vehicle.yCenter,        # y position
+# vehicle.xVelocity,      # x velocity
+# vehicle.yVelocity,      # y velocity
+# vehicle.heading,        # heading
+# vehicle.width,          # width
+# vehicle.length          # length
 X,Y,metadata = create_prediction_dataset(dataset, 200)
 
 output_path = joinpath(@__DIR__,"..", "res", "prediction_dataset_size$(length(X)).jld2")
